c4ab43c634d6fce4e0f40946004f99cad2b3be4c
zacharyburleson1 <93451464+zacharyburleson1@users.noreply.github.com>
2021-10-31 09:46:38 -0500

    Update HelloWorld.py
    Prints "Hello World!" in 3 different ways. Practicing committing changes.

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,6 +1,28 @@
-# Displays "Hello World!" and waits for the user to press the
-# enter key before the program terminates.
+# Author: Zachary Burleson
+# Title: Hello World
+# Date: 10/30/2021
+# Description: Displays "Hello World!" and waits for the user to press
+#       the enter key before the program terminates.
 
-print("Hello World!")
+# initialize variable used to print message
+hello = "Hello World!"
+count = 5
+
+# prints a message multiple times
+def print_many_times(message, times):
+    for _ in range(times):
+        print(hello)
+
+# print message once
+print(hello)
+print("\n\n")
+
+# print message many times
+print_many_times(hello, count)
+print("\n\n")
+
+# print every letter in message on new line
+for x in hello:
+    print("\t" + x + "\n")
 
 input("\nPress enter to exit.")
